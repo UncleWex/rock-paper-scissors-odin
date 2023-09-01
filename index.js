31e27f34a390794c0a55f81638bbad7fd7a1ad82
UncleWex <55881429+UncleWex@users.noreply.github.com>
2023-09-01 09:44:55 +0600

    add game logic

--- index.js
+++ index.js
@@ -6,3 +6,41 @@ let getComputerChoice = () => {
         default: return "Unknown";
     }
 }
+
+let playRound = (playerChoice, computerChoice) => {
+    let playerChoiceLower = String(playerChoice).toLowerCase();
+    let computerChoiceLower = String(computerChoice).toLowerCase();
+    if (playerChoiceLower === "rock") {
+        if(computerChoiceLower === "rock") return "Draw! Rock against rock"
+        if(computerChoiceLower === "paper") return "You lose! Paper beats rock"
+        if(computerChoiceLower === "scissors") return "You win! Rock beats scissors"
+    }
+    else if (playerChoiceLower === "paper") {
+        if(computerChoiceLower === "rock") return "You win! Paper beats rock"
+        if(computerChoiceLower === "paper") return "Draw! Paper against paper"
+        if(computerChoiceLower === "scissors") return "You lose! Scissors beat paper"
+    }
+    else if (playerChoiceLower === "scissors") {
+        if(computerChoiceLower === "rock") return "You lose! Rock beats scissors"
+        if(computerChoiceLower === "paper") return "You win! Scissors beat paper"
+        if(computerChoiceLower === "scissors") return "Draw! Scissors against scissors"
+    }
+    else return "Unknown input";
+}
+
+let checkInput = (input) => {
+    switch (String(input).toLowerCase()) {
+        case "rock" : return true;
+        case "paper" : return true;
+        case "scissors" : return true;
+        default : return false;
+    }
+}
+
+let game = () => {
+    while (true) {
+        const playerChoice = prompt("Choose your weapon", "Rock");
+        if (!checkInput(playerChoice)) break;
+        console.log(playRound(playerChoice, getComputerChoice()));
+    }
+}
