126b8299d74bad854855e3572899f9a458aefb90
UncleWex <55881429+UncleWex@users.noreply.github.com>
2023-09-01 08:54:30 +0600

    add project structure

---
+++ index.js
@@ -0,0 +1,8 @@
+let getComputerChoice = () => {
+    switch(Math.floor(Math.random() * 3)) {
+        case 0: return "Rock";
+        case 1: return "Paper";
+        case 2: return "Scissors";
+        default: return "Unknown";
+    }
+}
